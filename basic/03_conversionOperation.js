84f7b653a5a87b608ba1e6607987343e6b5e654f
Nidhi724 <nknidhi571@gmail.com>
2025-05-14 11:08:40 +0000

    Conversion of Datatypes

--- basic/03_conversionOperation.js
+++ basic/03_conversionOperation.js
@@ -13,3 +13,44 @@ undefined -> NaN
 if there is any string that can't be converted into no then it will show NaN
 
 */
+
+// ***************************** Operations ****************************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
+
+let str1 = "Hello"
+let str2 = "Nidhi"
+
+let str3 = str1 +str2
+console.log(str3);
+
+console.log("1" + 2);//12
+console.log(1 + "2");//12
+console.log("1" + 2 +2);//122
+console.log(1 + 2 +"2");//32
+
+// console.log(+true); -> 1
+// console.log(+""); ->0
+
+let num1 , num2, num3
+num1 = num2 = num3 = 2+2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+
+
+
+
+
