41ed640fc4d3f62058531af95eabddc9a2cbeab4
Nidhi724 <nknidhi571@gmail.com>
2025-05-17 12:06:16 +0000

    01Arrays

---
+++ 02_basic/01_array.js
@@ -0,0 +1,45 @@
+ //array
+
+//const myArr = [0,1,2,3,4,5,true,"hitesh"]
+//we can include various data type in a single array (resizable)
+
+const myArr = [0, 1, 2, 3, 4, 5]
+const myHeros = ["shaktiman" , "naagraj"]
+
+const myArr2 = new Array(1, 2, 3, 4)
+// console.log(myArr[1]);
+
+//Array methods
+
+// myArr.push(6)
+// myArr.push(7)
+// console.log(myArr);
+
+
+// unshift()-> used to add an elemnt at the zero index of an array
+// shift()-> uesd to remove the index element
+// myArr.unshift(9)
+// console.log(myArr);
+// myArr.shift()
+
+
+// console.log(myArr.includes(9));
+// // includes()-> says element is present or not
+// console.log(myArr.indexOf(3));
+// indexOf()-> gives the index of the current element
+
+
+const newArr = myArr.join()
+
+//join() -> convert the whole array in String and join them 
+
+// console.log(myArr);
+// console.log(newArr);
+
+
+// slice , splice
+
+// slice -> includes the staring elementa to n-1 element
+// splice -> includes 1-n elements in an array
+
+// only diffrence is splice channges the original array 
